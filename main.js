32d34a6361cfe1eef678d390597dc710ddd55412
phamboola <krstlphm@gmail.com>
2024-09-16 11:57:04 +1000

    testing java

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 if (window.screen.width > 780) {
     document.write( 
-    '<script src ="/js/cursor.js"><\/script>' +
-    '<script src ="/js/cursor_hover.js"><\/script>' +
+    '<script src ="./js/cursor.js"><\/script>' +
+    '<script src ="./js/cursor_hover.js"><\/script>' +
     '<script src ="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"><\/script>' +
-    '<script type="/js/script.js"><\/script>' 
+    '<script type="./js/script.js"><\/script>' 
     )
   }
