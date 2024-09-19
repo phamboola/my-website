43d6064112b5ae11c012f47459c59fa3b1f1b88f
phamboola <krstlphm@gmail.com>
2024-09-19 15:11:10 +1000

    update updates

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ else {
             scrub: true,
             onUpdate: (self) => {
               let progress = self.progress;
-              let newHeight = 150 + 350 * progress;
+              let newHeight = 200 + 150 * progress;
               gsap.to(service, {
                 height: newHeight + "px",
                 duration: 0.1,
