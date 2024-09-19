81aa549d456e72eace1f991b27f91ba09895558b
phamboola <krstlphm@gmail.com>
2024-09-19 15:03:31 +1000

    responsive java lmao its all about the ifs and else

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+Object.defineProperty(window, 'mobile', {
+  get() {
+    return window.innerWidth <= 767;
+  }
+});
+
+
+if (mobile) {
 document.addEventListener("DOMContentLoaded", function () {
   const lenis = new Lenis();
 
@@ -34,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
           scrub: true,
           onUpdate: (self) => {
             let progress = self.progress;
-            let newWidth = 30 + 70 * progress;
+            let newWidth = 100 + 0 * progress;
             gsap.to(imgContainer, {
               width: newWidth + "%",
               duration: 0.1,
@@ -50,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
           scrub: true,
           onUpdate: (self) => {
             let progress = self.progress;
-            let newHeight = 200 + 300 * progress;
+            let newHeight = 250 + 250 * progress;
             gsap.to(service, {
               height: newHeight + "px",
               duration: 0.1,
@@ -71,37 +80,80 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-   if(screen.width < 768){
-    
-            ScrollTrigger.create({
-              trigger: service,
-              start: "bottom bottom",
-              end: "top top",
-              scrub: true,
-              onUpdate: (self) => {
-                let progress = self.progress;
-                let newWidth = 150 + 70 * progress;
-                gsap.to(imgContainer, {
-                  width: newWidth + "%",
-                  duration: 0.1,
-                  ease: "none",
-                });
-              },
-            });
-    
-            ScrollTrigger.create({
-              trigger: service,
-              start: "top bottom",
-              end: "top top",
-              scrub: true,
-              onUpdate: (self) => {
-                let progress = self.progress;
-                let newHeight = 100 + 300 * progress;
-                gsap.to(service, {
-                  height: newHeight + "px",
-                  duration: 0.1,
-                  ease: "none",
-                });
-              },
-            });
-          };
+ }
+
+else {
+  document.addEventListener("DOMContentLoaded", function () {
+    const lenis = new Lenis();
+  
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+  
+    lenis.on("scroll", ScrollTrigger.update);
+  
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+  
+    gsap.ticker.lagSmoothing(0);
+  
+    const services = gsap.utils.toArray(".service");
+  
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+  
+    const observerCallback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const service = entry.target;
+          const imgContainer = service.querySelector(".img");
+  
+          ScrollTrigger.create({
+            trigger: service,
+            start: "bottom bottom",
+            end: "top top",
+            scrub: true,
+            onUpdate: (self) => {
+              let progress = self.progress;
+              let newWidth = 30 + 70 * progress;
+              gsap.to(imgContainer, {
+                width: newWidth + "%",
+                duration: 0.1,
+                ease: "none",
+              });
+            },
+          });
+  
+          ScrollTrigger.create({
+            trigger: service,
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            onUpdate: (self) => {
+              let progress = self.progress;
+              let newHeight = 150 + 350 * progress;
+              gsap.to(service, {
+                height: newHeight + "px",
+                duration: 0.1,
+                ease: "none",
+              });
+            },
+          });
+  
+          observer.unobserve(service);
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+    services.forEach((service) => {
+      observer.observe(service);
+    });
+  });
+
+}
